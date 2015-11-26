2bbe736b0e893719e1aeb20b4c20b46d9930cc39
Warren Haskins <warrenhaskins@gmail.com>
2015-11-26 17:37:58 +0000

    log env

--- app/config/index.js
+++ app/config/index.js
@@ -9,8 +9,10 @@ var env = process.env.NODE_ENV || 'development',
 
 
 if ( env ==='development' ) {
+    console.log('running in Development Mode...')
     config = require('./development.js');
 } else {
+    console.log('running in Production Mode...')
     config = require('./production.js');
 }
 
