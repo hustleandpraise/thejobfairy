7cda4bc0f508abc62d11f0e5b141c7ed4941464b
Warren Haskins <warrenhaskins@gmail.com>
2015-11-26 17:46:02 +0000

    fixed knex file to use config

--- knexfile.js
+++ knexfile.js
@@ -1,47 +1,17 @@
 // Update with your config settings.
 
+var Config = require('./app/config');
+
 module.exports = {
 
   development: {
     client: 'mysql',
-    connection: {
-        host     : '127.0.0.1',
-        user     : 'root',
-        password : 'root',
-        database : 'jobfairy_development'
-    }
-  },
-
-  staging: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
-    }
+    connection: Config.db
   },
 
   production: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
-    }
+    client: 'mysql',
+    connection: Config.db
   }
 
 };
