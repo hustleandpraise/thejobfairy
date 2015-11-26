8bace5f264d6b1ae31bb2e2ea446f0cca756370f
Warren Haskins <warrenhaskins@gmail.com>
2015-11-26 21:25:02 +0000

    cleaned

--- shipitfile.js
+++ shipitfile.js
@@ -16,36 +16,17 @@ module.exports = function (shipit) {
             servers: 'root@178.62.85.145'
         }
     });
-    
-    // shipit.task('build', function () {
-    // shipit.emit('built');
-    // });
 
-    // shipit.on('built', function () {
-    //     return shipit.remote('cd ~/apps/current').then((res) => {
-
-    //         return shipit.remote('sudo npm install').then((res) => {
-    //             shipit.remote('knex migrate:latest')
-    //             shipit.remote('forever start ./bin/www')
-    //         });
-                
-    //     }).then((done) => {
-    //         console.log(done)
-    //     })
-    //     console.log('done!')
-    // });
 
     shipit.on('published', () => {
-        //return shipit.remote('cd ./app/current').then((res) => {
 
-            return shipit.remote('cd ./app/current && sudo npm install').then((res) => {
-                shipit.remote('cd ./app/current && knex migrate:latest')
-                shipit.remote('cd ./app/current forever start ./bin/www')
-            });
+        console.log('Done!')
+
+        // return shipit.remote('cd ./app/current && NODE_ENV=production sudo npm install').then((res) => {
+        //     shipit.remote('cd ./app/current && NODE_ENV=production knex migrate:latest')
+        //     shipit.remote('cd ./app/current && NODE_ENV=production forever start ./bin/www')
+        // });
                 
-        //}).then((done) => {
-            console.log(done)
-        //})
     });
 
 
