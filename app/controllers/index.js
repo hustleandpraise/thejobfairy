343619e872d59cf49d22d6a4101c890a5e5a4e32
Warren Haskins <warrenhaskins@gmail.com>
2015-12-01 19:35:05 +0000

    new index queries

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,7 +1,19 @@
 
 var express = require('express'),
     router  = express.Router(),
-    models  = require('../models');
+    models  = require('../models'),
+    _       = require('lodash'),
+    Moment  = require('moment'),
+    services = require('../services');
+
+
+var linkify_tweet = function(str) {
+    var tweet = str.replace(/(\b(https?|ftp|file):\/\/[-A-Z0-9+&@#\/%?=~_|!:,.;]*[-A-Z0-9+&@#\/%=~_|])/ig,"<a href='$1'>$1</a>");
+    tweet = tweet.replace(/(^|\s)@(\w+)/g, "$1<a href=\"http://www.twitter.com/$2\">@$2</a>");
+    return tweet.replace(/(^|\s)#(\w+)/g, "$1<a href=\"http://search.twitter.com/search?q=%23$2\">#$2</a>");
+};
+
+
 
 /*
 |--------------------------------------------------------------------------
@@ -11,14 +23,53 @@ var express = require('express'),
 
 router.get('/', (req, res, next) => {
 
-    // var tweets = new models.Tweet().query('limit', 10).fetchAll({ withRelated: ['user', 'tags', 'categories', 'locations'] });
-    var getTags        = new models.Tag().query('orderBy', 'title', 'asc').fetchAll({ withRelated: ['tweets'] });
-    var getCategories  = new models.Category().query('orderBy', 'title', 'asc').fetchAll({ withRelated: ['tweets'] });
-    var getLocations   = new models.Location().query('orderBy', 'title', 'asc').fetchAll({ withRelated: ['tweets'] });
+    var perPage = 28,
+        page = req.query.page > 0 ? req.query.page : 0;
 
-    Promise.all([getTags, getCategories, getLocations]).then((models) => {
-        // return res.json(models)
-        res.render('index', { tags: models[0].toJSON(), categories: models[1].toJSON(), locations: models[2].toJSON() });
+    console.log(req.query)
+
+
+    var tweets = new models.Tweet().query(function(q) {
+        q.orderBy('created_at', 'desc');
+        q.limit(perPage);
+        return q.offset(perPage * page);
+    }).fetchAll({ withRelated: ['user'] });
+
+    var getTags        = new models.Tag().query('orderBy', 'title', 'asc').fetchAll();
+    var getCategories  = new models.Category().query('orderBy', 'title', 'asc').fetchAll();
+    var getLocations   = new models.Location().query('orderBy', 'title', 'asc').fetchAll();
+    var getTweetCount  = new models.Tweet().count();
+
+    
+
+    Promise.all([getTags, getCategories, getLocations, tweets, getTweetCount]).then((models) => {
+
+        var tweets = models[3].toJSON().map((tweet) => {
+            return _.assign(tweet, {
+                text: linkify_tweet(tweet.text),
+                color: services.Str.getColor()
+            })
+        });
+
+        var tags = models[0].toJSON().map((tag) => {
+            return _.assign(tag, {
+                color: services.Str.getColor()
+            })
+        });
+
+        var count = models[4];
+
+        res.render('index', { 
+            tags: tags, 
+            categories: models[1].toJSON(), 
+            locations: models[2].toJSON(), 
+            tweets: tweets, 
+            moment: Moment,
+            count: count,
+            page: page,
+            pages: count / perPage
+        });
+
     })
 
 });
