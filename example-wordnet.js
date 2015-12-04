47188d2fc3a2ba6c5560301d1cbdb988155a1471
Warren Haskins <warrenhaskins@gmail.com>
2015-12-04 19:42:23 +0000

    added wordnet example

---
+++ example-wordnet.js
@@ -0,0 +1,70 @@
+// @CallMe_2Short Automate Recruiting. Simplify Job Search. https://t.co/QPWR1TCCxB @HireTeamMate #hireme #jobfairy
+{ 
+	nouns: [ 'Job', 'Search', 'co' ],
+	verbs: [ 'Automate', 'Simplify', 'Job', 'Search' ],
+	adjectives: [],
+	adverbs: [],
+	rest: [ 	'QPWR1TCCxB', 'CallMe_2Short', 'Recruiting', 'jobfairy', 'https', 'HireTeamMate', 'hireme' ] 
+}
+
+// Au Pair wanted in Dublin 13 https://t.co/IBXECPnMjq #jobfairy #aupair
+{ 
+	nouns: [ 'Au', 'Pair', 'Dublin', '13', 'co' ],
+	verbs: [ 'Pair' ],
+	adjectives: [ 'wanted', '13' ],
+	adverbs: [],
+	rest: [ 'IBXECPnMjq', 'aupair', 'https', 'jobfairy' ] 
+}
+
+
+// RT @Fenergo: Visit the Fenergo team at today's Trinity graduate careers event! #jobfairy #graduates #careers #dublin #internships https://t…
+{ 
+	nouns: [ 'Visit', 'team', 'today', 'Trinity', 'graduate', 'event', 'dublin' ],
+	verbs: [ 'Visit', 'team', 'graduate' ],
+	adjectives: [ 'graduate' ],
+	adverbs: [ 'today' ],
+	rest: [ 'RT', 'Fenergo', 'graduates', 'careers', 'jobfairy', 'internships', 'https' ] 
+}
+
+
+//Enthusiastic &amp; energetic Head Chef needed at @spillanesbar to run their busy seaside, seasonal kitchen. https://t.co/NZQWRM9VEM #jobfairy
+{ 
+	nouns: [ 'amp', 'Head', 'Chef', 'run', 'seaside', 'seasonal', 'kitchen', 'co' ],
+	verbs: [ 'Head', 'run', 'busy' ],
+	adjectives: [ 'Enthusiastic', 'energetic', 'needed', 'busy', 'seasonal' ],
+	adverbs: [],
+	rest: [ 'NZQWRM9VEM', 'spillanesbar', 'https', 'jobfairy' ] 
+}
+
+
+
+// Interested in this job? DIGITAL RECRUITMENT CONSULTANT in Dublin, Ireland https://t.co/Ivg1dTwoFA #jobfairy
+{ 
+	nouns: [ 'job', 'RECRUITMENT', 'CONSULTANT', 'Dublin', 'Ireland', 'co' ],
+	verbs: [ 'job' ],
+	adjectives: [ 'Interested', 'DIGITAL' ],
+	adverbs: [],
+	rest: [ 'Ivg1dTwoFA', 'jobfairy', 'https' ] 
+}
+
+
+
+// Enthusiastic &amp; energetic Head Chef needed at @spillanesbar to run their busy seaside, seasonal kitchen. https://t.co/SVJsp0Ps8B #jobfairy
+{ 
+	nouns: [ 'amp', 'Head', 'Chef', 'run', 'seaside', 'seasonal', 'kitchen', 'co' ],
+	verbs: [ 'Head', 'run', 'busy' ],
+	adjectives: [ 'Enthusiastic', 'energetic', 'needed', 'busy', 'seasonal' ],
+	adverbs: [],
+	rest: [ 'SVJsp0Ps8B', 'spillanesbar', 'https', 'jobfairy' ] 
+}
+
+
+
+// RT @CapitalHRGroup: #jobfairy Accounts Technician/Admin for our global client in Sth #Dublin https://t.co/MQjQn8milf #irishbizparty https:/…
+{ 
+	nouns: [ 'Technician', 'client', 'Sth', 'Dublin', 'co' ],
+	verbs: [],
+	adjectives: [ 'global' ],
+	adverbs: [],
+	rest: [ 'RT', 'MQjQn8milf', 'CapitalHRGroup', 'jobfairy', 'Accounts', 'Admin', 'https', 'irishbizparty' ] 
+}
